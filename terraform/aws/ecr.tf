1e4da90fa8ee8aaca60bcb1a9bfd7c79fd5f7818
caswalker <90857961+caswalker@users.noreply.github.com>
2021-12-17 12:25:34 -0500

    Update ecr.tf

--- terraform/aws/ecr.tf
+++ terraform/aws/ecr.tf
@@ -1,5 +1,5 @@
 resource aws_ecr_repository "repository" {
-  name                 = "${local.resource_prefix.value}-repository"
+  name                 = "${local.resource_prefix.value}-repository" 
   image_tag_mutability = "MUTABLE"
 
   tags = merge({
@@ -23,7 +23,7 @@ locals {
 
 resource null_resource "push_image" {
   provisioner "local-exec" {
-    working_dir = "${path.module}/resources"
+    working_dir = "${path.module}/resources" 
     command     = <<BASH
     aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com
     docker build -t ${aws_ecr_repository.repository.name} .
